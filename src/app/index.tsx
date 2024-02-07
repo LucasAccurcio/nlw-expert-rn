7a38bc0739cee784c0dc6f252c313c6fe9d4d3c4
Lucas G Accurcio Ferreira <lucas.ferreira@brisabr.com.br>
2024-02-07 16:24:05 -0300

    feat: atualizações de locais de arquivos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native';
+import { Header } from '@/components/header';
+import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View className="flex-1 pt-8">
+      <Header title={"Faça seu pedido"} />
+      {/* <Text className="text-white text-2xl font-heading">Hello World</Text> */}
     </View>
   )
 }
